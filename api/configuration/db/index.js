84f3b8ec517bb0cd84e1d4784fdb1ed5bb02ebf9
Nizom Muhammadov <nizom702@gmail.com>
2020-10-29 15:34:48 +0500

    d

--- api/configuration/db/index.js
+++ api/configuration/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new  Sequelize('mobilset','postgres', 'root123',{
-    host: "localhost",
+const sequelize = new  Sequelize('de2r7hno9ss8dc','ibmatkkhlvwana', 'a402ecf2eb0ab1063a1b344193e835e811dd7f581ad28b14a15a74c40e758081',{
+    host: "ec2-34-231-56-78.compute-1.amazonaws.com",
     dialect: "postgres"
 })
 module.exports = sequelize;
